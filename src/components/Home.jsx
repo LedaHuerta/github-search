7f4fb45bd839955800dc1ed718f022d186fe68d6
LedaHuerta <ledamhuerta@gmail.com>
2021-03-22 06:00:34 -0600

    adding welcome message at home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import image from '../assets/octocat.png';
 
 
 
@@ -8,8 +9,23 @@ const Home = () => {
 
     return(
         <Fragment>
-            <h2>Home</h2>
-        </Fragment>
+          <div className="container">
+            <div className="row justify-content-center align-items-center">
+                <div className="col-md-6 mt-5 text-center">
+                    <div className="card">
+                        <div className="card-header">
+                            <h2>Welcome to Github Search</h2>
+                        </div>
+                        <div className="card-body">
+                            <h3>Find users and repos fast</h3>
+                            <img src={image} alt="user" className="img-fluid" />
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+      </Fragment>  
     );
 }
 
