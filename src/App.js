ec64c1c9d9be5a2587838b7f52cb3a54644e113f
LedaHuerta <ledamhuerta@gmail.com>
2021-03-22 05:27:42 -0600

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GithubUsers from './components/GithubUsers';
+import GithubRepos from './components/GithubRepos';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import './index.css';
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <Router>
+      <NavBar/>
+      <div>
+        <Switch>
+          <Route exact path="/users" component={GithubUsers}/>
+          <Route exact path="/repos" component={GithubRepos}/>
+          <Route exact path="/github-search" component={Home}/>
+          <Route path="/" component={Home}/>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
