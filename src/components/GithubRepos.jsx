c744f05fc3ad168a9a0acc4d6eda6c41857a1d7b
LedaHuerta <ledamhuerta@gmail.com>
2021-03-22 08:03:32 -0600

    adding repos search

--- src/components/GithubRepos.jsx
+++ src/components/GithubRepos.jsx
@@ -1,14 +1,68 @@
 import React, { Fragment, useState } from 'react';
-
+import Repos from './Repos';
+import NotFound from './NotFound';
+import { axios } from "./axios";
+import { clientId, clientSecret } from './GithubCredentials';
 
 
 const GithubRepos = () => {
+    const[updateInput, setUpdateInput] = useState('');
+    const[repos, setRepos] = useState(null);
+    
+    //get the username from user
+    const handleChange = (e) => {
+        setUpdateInput(e.target.value)
+    };
 
-
+    //search repos
+    const getRepos = async () => {
+        const responseRepos = await axios
+        .get( `${updateInput}/repos?clientId=${clientId}&clientSecret=${clientSecret}` )
+        .catch((err) => {console.log("Error:", err)})
+        if(responseRepos && responseRepos.data) {
+            setRepos(responseRepos.data)
+        }
+        
+    };
+   
 
     return(
         <Fragment>
-            <h2>repos</h2>
+            <div className="container mt-3">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header bg-info text-white">
+                                <h3>Github Repos</h3>
+                            </div>
+                            <div className="card-body">
+                                <div className="form-group mr-2">
+                                    <input
+                                        value = {updateInput}
+                                        onChange = {handleChange}
+                                        size="40"
+                                        type="text" 
+                                        className="form-control" 
+                                        placeholder="User Name"/>
+                                </div>
+                                <div>
+                                    <button onClick={getRepos} className="btn btn-secondary btn-md">Search</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="container mt-3">
+                        <div className="row">
+                            <div className="col">
+                                {
+                                    repos ? <Repos repos={ repos }/> :  <NotFound/>
+                                }
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Fragment>
     );
 }
---
+++ src/components/Repos.jsx
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+
+
+
+const Repos = (props) => {
+    let { repos } = props;
+    return(
+        <Fragment>
+            <div className="card">
+                <div className="card-header bg-light text-dark">
+                    <h3>Github Repos</h3>
+                </div>
+                <div className="card-body">
+                    <table className="table table-hover text-left justify-content-around">
+                        <thead className="bg-info">
+                            <tr>
+                                <th>Repo Name</th>
+                                <th>Stars</th>
+                                <th>Watchers</th>
+                            </tr>
+                        </thead>
+                            
+                        <tbody>
+                            {
+                            repos.map((repo) => {
+                                    return(
+                                        <tr>
+                                            <td><a href={repo.html_url} target="_blank" rel="noreferrer" className="text-dark">{ repo.name }</a></td>
+                                            <td><span className="badge badge-success mx-2" >{ repo.stargazers_count } Stars</span></td>
+                                            <td><span className="badge badge-warning mx-2">{ repo.watchers_count } Watches</span></td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                            
+                    </table>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Repos;
