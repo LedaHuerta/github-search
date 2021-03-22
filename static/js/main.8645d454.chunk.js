b2709804978bf640ba58f08c5aff9ffc108405c8
LedaHuerta <ledamhuerta@gmail.com>
2021-03-22 05:09:04 -0600

    Updates

---
+++ static/js/main.8645d454.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgithub-search"]=this["webpackJsonpgithub-search"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),r=n.n(c),i=n(3),s=n.n(i),o=(n(8),n(9),n(0));var h=function(){return Object(o.jsx)("div",{children:"hello worlds"})};s.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(h,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.8645d454.chunk.js.map
