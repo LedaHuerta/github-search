eae0eddbae662f14f2d7a4a447b2a99e5a1b0c5e
LedaHuerta <ledamhuerta@gmail.com>
2021-03-22 13:37:34 -0600

    navbar style adjusts

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,25 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import image from '../assets/octocat2.png';
+
 
 const NavBar = () => {
     return(
         <Fragment >
-            <nav className="navbar row bg-dark justify-content-start">
-                <Link to ="/github-search" className="navbar-brand bg-dark col-2 text-white text-left ml-2">Home</Link>
-                <Link to="/users" className="navbar-brand bg-dark col-2 text-white text-left">Profile</Link>
-                <Link to ="/repos" className="navbar-brand bg-dark col-2 text-white text-left">Repos</Link>
+            <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+                <Link to ="/github-search" className="navbar-brand">
+                    <img src={image} alt="octocat width" className="d-inline-block align-bottom mr-2"/>
+                    GitHub Search
+                </Link>
+                <div className="collapse navbar-collapse mt-2" id="navbarToggler">
+                    <div className="navbar-nav ">
+                        <Link to="/users" className="nav-item nav-link">Profile</Link>
+                        <Link to ="/repos" className="nav-item nav-link">Repos</Link>
+                    </div>
+                </div>
             </nav>
         </Fragment>
     );
